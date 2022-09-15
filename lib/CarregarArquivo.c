1b7933e2873fd935309aa212e6dac07a3b63e936
João Pedro Morbeck <jpmorbeck45@gmail.com>
2022-09-14 23:28:09 -0300

    Abrir e ler arquivo funcionando

--- lib/CarregarArquivo.c
+++ lib/CarregarArquivo.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-void CarregarArquivo() {
+void CarregarArquivo(FILE *arquivo) {
     
+    char string[300];
+    char ch;
+    int i = 1;
+
+    fscanf(arquivo, "%[^\n]", string);
+    ch = fgetc(arquivo);
+
+    while(1){
+        fscanf(arquivo, "%[^\n]", string);
+        ch = fgetc(arquivo);
+        if(ch == EOF){
+            break;
+        }
+
+        printf(string);
+        printf("\n");
+
+    }
+ 
 }
--- main.c
+++ main.c
@@ -3,30 +3,51 @@
 #include "lib/EmitirRelatorio.c"
 #include "lib/FecharAplicacao.c"
 
-
 int main(){
 
-    char opcao;
+    char opcao, nomeArquivo[50];
+    FILE *arquivo = NULL;
 
     while(opcao != '3'){
 
-        printf("\n\n1 - Carregar Arquivo de Dados\n");
+        printf("\n1 - Carregar Arquivo de Dados\n");
         printf("2 - Emitir Relatório\n");
-        printf("3 - Sair da Aplicação\n\n");
+        printf("3 - Fechar Aplicação\n\n");
         
         scanf("%c", &opcao);
         getchar();
 
         switch(opcao){
             case '1':
-                CarregarArquivo();
+
+                printf("\nInforme o nome do arquivo:");
+                scanf("%s", nomeArquivo);
+                getchar();
+
+                arquivo = fopen(nomeArquivo, "r");
+                if(arquivo == NULL){
+                    printf("Ocorreu um erro na abertura do arquivo, tente novamente\n\n");
+                    break;
+                }
+
+                CarregarArquivo(arquivo);
+                fclose(arquivo);
+                arquivo = NULL;
+
                 break;
 
             case '2':
+
+                if(arquivo == NULL){
+                    printf("\nArquivo Vazio!!!\n\n");
+                    break;
+                }
+
                 EmitirRelatorio();
                 break;
                 
             case '3':
+                
                 FecharAplicacao();
                 break;
         }
